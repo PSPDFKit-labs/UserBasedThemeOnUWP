720b9c80d59097cab7f31e936e17359b6bc4e924
Shantanu Methikar <smethikar18@gmail.com>
2023-08-20 14:00:23 +0530

    Add sample project

---
+++ UserBasedTheme/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using PSPDFKit.UI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UserBasedTheme
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void OnUser1Click(object sender, RoutedEventArgs e)
+        {
+            PdfContainer.Content = new PdfView()
+            {
+                Css = new Uri("ms-appx-web:///Assets/user1.css"),
+                PdfUriSource = new Uri("ms-appx:///Assets/demo.pdf")
+            };
+        }
+
+        private void OnUser2Click(object sender, RoutedEventArgs e)
+        {
+            PdfContainer.Content = new PdfView()
+            {
+                Css = new Uri("ms-appx-web:///Assets/user2.css"),
+                PdfUriSource = new Uri("ms-appx:///Assets/demo.pdf")
+            };
+        }
+    }
+}
